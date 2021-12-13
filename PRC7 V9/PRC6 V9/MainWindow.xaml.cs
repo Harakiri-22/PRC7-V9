b7b0d84eaed6f9520f8dd9d8042ed8b9e71ddc5d
Mishin Dmitiriy <93830209+Harakiri-22@users.noreply.github.com>
2021-12-13 11:29:34 +0300

    Add files via upload

---
+++ PRC7 V9/PRC6 V9/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRC7_V9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Мишин Д.А. ИСП-34", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void CompareTime_Click(object sender, RoutedEventArgs e)
+        {
+            Time timeOne = new Time
+            {
+                Hour = Convert.ToInt32(hour1.Text),
+                Minute = Convert.ToInt32(minute1.Text),
+                Second = Convert.ToInt32(second1.Text)
+            };
+
+            Time timeTwo = new Time
+            {
+                Hour = Convert.ToInt32(hour2.Text),
+                Minute = Convert.ToInt32(minute2.Text),
+                Second = Convert.ToInt32(second2.Text)
+            };
+
+            bool result;
+            result = Time.TimeCompare(timeOne.Hour, timeOne.Minute, timeOne.Second, timeTwo.Hour, timeTwo.Minute, timeTwo.Second);
+
+            if (result == true)
+            {
+                MessageBox.Show("Моменты времени равны");
+            }
+            else
+            {
+                MessageBox.Show("Моменты времени неравны");
+            }
+        }
+    }
+}
---
+++ PRC7 V9/PRC6 V9/Time.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PRC7_V9
+{
+    class Time
+    {
+        private int _hour;
+        private int _minute;
+        private int _second;
+
+        public static bool TimeCompare(int hour, int minute, int second, int hour1, int minute1, int second1)
+        {
+            bool result = hour == hour1 && minute == minute1 && second == second1;
+            return result;
+        }
+
+        public int Hour
+        {
+            get => _hour;
+
+            set
+            {
+                if (value % 2 == 1 && value < 24)
+                {
+                    _hour = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введены не корректные данные");
+                }
+            }
+        }
+
+        public int Minute
+        {
+            get => _minute;
+
+            set
+            {
+                if (value % 2 == 1 && value < 60)
+                {
+                    _minute = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введены не корректные данные");
+                }
+            }
+        }
+
+        public int Second
+        {
+            get => _second;
+
+            set
+            {
+                if (value % 2 == 1 && value < 60)
+                {
+                    _second = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введены не корректные данные");
+                }
+            }
+        }
+    }
+}
